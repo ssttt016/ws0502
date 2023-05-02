af16176fd57b559e59f72d86fb733b14c4b346b9
ssttt016 <ssttt016@gmail.com>
2023-05-02 17:46:39 +0900

    Initial commit

---
+++ src/main/java/com/kbstar/controller/ItemController.java
@@ -0,0 +1,18 @@
+package com.kbstar.controller;
+
+import com.kbstar.dto.Item;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@RequestMapping("/item")
+public class ItemController {
+
+
+}
+
